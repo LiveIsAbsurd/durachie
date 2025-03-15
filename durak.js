46df377c50f414e3da9be54fcd746cc83db0dc79
LiveIsAbsurd <anndreybykov19@gmail.com>
2025-03-15 22:33:31 +0300

    фикс

--- durak.js
+++ durak.js
@@ -35,7 +35,7 @@ bot.on('message', (msg, match) => {
     if (msg.text[0] != '/') {
         const replyBot = reply?.from.id == "7770648727"
         // const replyBot = reply?.from.id == "7739320318"
-        const sendTrig = replyBot ? true : Math.random() < 0.1;
+        const sendTrig = replyBot || Math.random() < 0.1;
         console.log(reply?.from.id + ' ' + sendTrig);
 
         if (sendTrig) {
