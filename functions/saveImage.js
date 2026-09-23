ba4929965bb1ae07b7613022e46e0ae0608f90d5
LiveIsAbsurd <anndreybykov19@gmail.com>
2026-09-23 18:33:53 +0300

    обернул file в try/catch

--- functions/saveImage.js
+++ functions/saveImage.js
@@ -5,17 +5,20 @@ const saveImage = async (msg, bot, images) => {
     
     const photoId = msg.photo[msg.photo.length - 1].file_id;
 
-    // Получаем информацию о файле
-    const fileInfo = await bot.getFile(photoId);
-    const fileUrl = `https://api.telegram.org/file/bot${bot.token}/${fileInfo.file_path}`;
+    try {
+        const fileInfo = await bot.getFile(photoId);
+        const fileUrl = `https://api.telegram.org/file/bot${bot.token}/${fileInfo.file_path}`;
 
-    // Загружаем изображение и конвертируем его в буфер
-    const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
-    const imageBuffer = Buffer.from(response.data, 'binary');
+        const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(response.data, 'binary');
 
-    if (!images[msg.chat.id].includes(imageBuffer)) {
-        images[msg.chat.id].length > 75 ? images[msg.chat.id].shift() : null;
-        images[msg.chat.id].push(imageBuffer);  
+        if (!images[msg.chat.id].includes(imageBuffer)) {
+            images[msg.chat.id].length > 75 ? images[msg.chat.id].shift() : null;
+            images[msg.chat.id].push(imageBuffer);  
+        };
+    } catch(err) {
+        console.log(err);
+        return;
     }
 }
 
